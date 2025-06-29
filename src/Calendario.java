4aab290c41a141c2c6b346060100ac0693fccb81
Jean PauÃl <jpalbap@udistrital.edu.co>
2025-06-29 15:05:44 -0500

    calendario

--- src/Calendario.java
+++ src/Calendario.java
@@ -1,43 +1,46 @@
 import java.time.LocalDate;
 
 public class Calendario {
-    private String descripcion;
-    private LocalDate fecha;
-    private int usuarioId; // para relacionarlo con el usuario o la mascota si lo deseas
+    private LocalDate fechaBaño;
+    private LocalDate fechaCompraAlimentos;
+    private LocalDate fechaCumpleaños;
 
-
-    public Calendario(String descripcion, LocalDate fecha, int usuarioId) {
-        this.descripcion = descripcion;
-        this.fecha = fecha;
-        this.usuarioId = usuarioId;
+    // Constructor
+    public Calendario(LocalDate fechaBaño, LocalDate fechaCompraAlimentos, LocalDate fechaCumpleaños) {
+        this.fechaBaño = fechaBaño;
+        this.fechaCompraAlimentos = fechaCompraAlimentos;
+        this.fechaCumpleaños = fechaCumpleaños;
     }
 
-
-    public String getDescripcion() {
-        return descripcion;
+    // Getters y Setters
+    public LocalDate getFechaBaño() {
+        return fechaBaño;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setFechaBaño(LocalDate fechaBaño) {
+        this.fechaBaño = fechaBaño;
     }
 
-    public LocalDate getFecha() {
-        return fecha;
+    public LocalDate getFechaCompraAlimentos() {
+        return fechaCompraAlimentos;
     }
 
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
+    public void setFechaCompraAlimentos(LocalDate fechaCompraAlimentos) {
+        this.fechaCompraAlimentos = fechaCompraAlimentos;
     }
 
-    public int getUsuarioId() {
-        return usuarioId;
+    public LocalDate getFechaCumpleaños() {
+        return fechaCumpleaños;
     }
 
-    public void setUsuarioId(int usuarioId) {
-        this.usuarioId = usuarioId;
+    public void setFechaCumpleaños(LocalDate fechaCumpleaños) {
+        this.fechaCumpleaños = fechaCumpleaños;
     }
 
     public String toString() {
-        return " Evento: " + descripcion + " | Fecha: " + fecha + " | Usuario ID: " + usuarioId;
+        return " Calendario de eventos:\n" +
+                "Fecha de baño: " + fechaBaño + "\n" +
+                "Fecha de compra de alimentos: " + fechaCompraAlimentos + "\n" +
+                "Cumpleaños: " + fechaCumpleaños;
     }
 }
