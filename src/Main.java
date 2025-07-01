032c27e6d817e992f0b7c2d87c819b0ecf1903df
Radkiil <dfcastroc@udistrital.edu.co>
2025-06-30 21:33:09 -0500

    Base

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-public class Main{
+import javax.swing.*;
 
+public class Main{
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Comet the pet");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,450);
+
+        JButton Boton1 = new JButton("Boton1");
+        JButton Boton2 = new JButton("Boton2");
+        JButton Boton3 = new JButton("Boton3");
+        JButton Boton4 = new JButton("Boton4");
+        frame.add(Boton1);
+
+        JLabel Label1 = new JLabel("Nombre");
+        frame.add(Label1);
+
+        frame.setVisible(true);
+    }
 
 }
---
+++ src/SwingUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class SwingUI {
+    private JButton INICIARSESIÓNButton;
+    private JButton CREARCUENTANUEVAButton;
+    private JTextField correoElectronicoTextField;
+    private JPasswordField contraseñaPasswordField;
+}
